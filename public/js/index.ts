05c2df78c532c0f96eb0488a3cbea61b5a7e05cd
David Ramírez <cdavidramirezg24@gmail.com>
2020-04-30 21:09:00 -0500

    Ejemplo de ads en vídeo

--- public/js/index.ts
+++ public/js/index.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from './media/MediaPlayer';
 import AutoPlay from './plugins/AutoPlay';
 import AutoPause from './plugins/AutoPause';
+import Ads from './plugins/ads';
 
 const video = document.querySelector('video');
 const play: HTMLElement = document.querySelector('#playButton');
@@ -8,7 +9,7 @@ const mute: HTMLElement = document.querySelector('#muteButton');
 
 const player = new MediaPlayer({
   el: video,
-  plugins: [new AutoPlay(), new AutoPause()],
+  plugins: [new AutoPlay(), new AutoPause(), new Ads()],
 });
 
 play.onclick = () => player.togglePlay();
